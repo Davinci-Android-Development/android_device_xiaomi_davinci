1023e3447114c672ed71bd9b6d4fb931533e1246
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:07:22 +0200

    davinci: Don't build vndk_package
    * It's built by default if vndk is set to current
    
    Change-Id: I2a00446046bafe265e5ac894656abc72514319d8

--- device.mk
+++ device.mk
@@ -156,10 +156,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.davinci
