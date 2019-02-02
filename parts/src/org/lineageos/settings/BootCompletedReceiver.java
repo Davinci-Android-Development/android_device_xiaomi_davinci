19f1f1b3001040365e70152bb2d4069467191242
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-02 14:31:51 -0200

    davinci: dirac: Only call setEnabled on boot completed
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -43,7 +43,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         PopupCameraUtils.startService(context);
         
-        new DiracUtils(context);
+        new DiracUtils(context).onBootCompleted();
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
