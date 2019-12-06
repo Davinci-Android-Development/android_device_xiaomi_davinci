8de2214d8c155872b3aa8fc5525678b29442b9bf
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    davinci: Use 6GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/davinci/davinci-vendor.mk)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
