f4ce2337ec577ade41485839af5bb80c17d01976
raysenlau <raysen@live.cn>
2019-12-14 09:00:34 +0800

    Revert "raphael: Build common Trust HAL"
    This reverts commit 8b093c32b21e2eba73de3f515bd2f4ad908dfc17.

--- device.mk
+++ device.mk
@@ -132,10 +132,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
