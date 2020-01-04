f316043f9e23559889150a33130dcb1d7ec48d9d
raysenlau <raysen@live.cn>
2020-01-04 21:25:41 +0800

    davinci: Don't build android.hidl.base@1.0 for now

--- device.mk
+++ device.mk
@@ -73,12 +73,6 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
 
 # HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest.xml
 
