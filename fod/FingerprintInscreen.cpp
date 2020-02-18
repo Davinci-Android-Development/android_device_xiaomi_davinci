881dee0dbab453c06986f358cc1980b281062575
mountaser <mountaser1998@gmail.com>
2020-02-18 19:09:19 +0000

    Revert "davinci: fod: Correct DISPPARAM name"
    This reverts commit adb2883cceb88638266b6faa053bd93c75d66248.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -28,13 +28,15 @@
 #define PARAM_NIT_FOD 3
 #define PARAM_NIT_NONE 0
 
-#define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
-#define DISPPARAM_HBM_FOD_ON "0x20000"
-#define DISPPARAM_HBM_FOD_OFF "0xE0000"
+#define FOD_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display/hbm"
+#define FOD_HBM_ON 1
+#define FOD_HBM_OFF 0
 
 #define FOD_STATUS_PATH "/sys/devices/virtual/touch/tp_dev/fod_status"
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
+
+#define FINGERPRINT_ERROR_VENDOR 8
 
 #define FOD_SENSOR_X 445
 #define FOD_SENSOR_Y 1931
@@ -83,13 +85,13 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_ON);
+    set(FOD_HBM_PATH, FOD_HBM_ON);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_FOD);
     return Void();
 }
 
 Return<void> FingerprintInscreen::onRelease() {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
+    set(FOD_HBM_PATH, FOD_HBM_OFF);
     xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
     return Void();
 }
