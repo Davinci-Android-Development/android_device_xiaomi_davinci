cb8e76503b3e1175f7f3d504eea85b1168708d34
mountaser <mountaser1998@gmail.com>
2020-05-06 18:22:16 +0000

    davinci: Havoc-ify

--- havoc_davinci.mk
+++ havoc_davinci.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := havoc_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
