b97d1e3a99810d5b563daa9c97fe056358dcdbea
mountaser <mountaser1998@gmail.com>
2020-05-06 18:22:16 +0000

    davinci: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/havoc_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    havoc_davinci-user \
+    havoc_davinci-userdebug \
+    havoc_davinci-eng
