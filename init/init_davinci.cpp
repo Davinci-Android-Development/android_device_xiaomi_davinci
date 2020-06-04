f7875bdf45688ba9fa7565a8c2bc9a0efa1e53bd
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-06-04 13:11:29 +0200

    davinci: init: upstream fp to coral june patches

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -109,7 +109,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
+    fingerprint = "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
