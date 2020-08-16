cb55e4fbfdef389912f123d10b413c68294876c9
mountaser <mountaser1998@gmail.com>
2020-08-16 02:06:39 +0000

    Revert "davinci: init: Set hardware sku to davinci"
    This reverts commit d0b39546ebb7a40c91eefaf51ccbd985f7e73a5b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,11 +106,6 @@ LOC_HIDL_VERSION := 3.0
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
-ODM_MANIFEST_SKUS += \
-    davinci
-
-ODM_MANIFEST_DAVINCI_FILES := $(DEVICE_PATH)/configs/hidl/manifest_davinci.xml
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
 TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
